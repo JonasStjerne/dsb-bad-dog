2af0070005b0b5af8db0f7f3ecb559bfb0280ae9
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-06-12 15:46:37 +0200

    emailFunction progress

---
+++ emailFunction/emailFunction/IReciept.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace emailFunction
+{
+    public interface IReciept
+    {
+        int OrderId { get; set; }
+        string Email { get; set; }
+        string DepSt { get; set; }
+        string DesSt { get; set; }
+        DateTime DepDateTime { get; set; }
+        DateTime DesDateTime { get; set; }
+
+    }
+}
--- emailFunction/emailFunction/inboxMonitor.cs
+++ emailFunction/emailFunction/inboxMonitor.cs
@@ -10,26 +10,20 @@ using Newtonsoft.Json;
 
 namespace emailFunction
 {
-    public static class emailFunction
+    public static class inboxMonitor
     {
-        [FunctionName("emailFunction")]
+        [FunctionName("inboxMonitor")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation(requestBody);
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(data);
         }
     }
 }
