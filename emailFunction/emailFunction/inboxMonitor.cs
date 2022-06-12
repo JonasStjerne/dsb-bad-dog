16d16d01a39d5700ff7598a6b8329593cc13387b
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-06-12 15:48:06 +0200

    Structure of function

--- emailFunction/emailFunction/inboxMonitor.cs
+++ emailFunction/emailFunction/inboxMonitor.cs
@@ -17,13 +17,39 @@ namespace emailFunction
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            log.LogInformation(requestBody);
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            return new OkObjectResult(data);
+            try
+            {
+                var Reciept = extractEmailContent(data["Text-part"]);
+            } catch(InvalidCastException e)
+            {
+                //If the cast throws exeption log it(a new email layout is properly used)
+                log.LogError(e.ToString());
+                //return 400
+                return new BadRequestResult();
+            }
+           
+
+            //Insert Reciept into db
+
+
+            return new OkResult();
+        }
+
+        public static IReciept extractEmailContent(dynamic content)
+        {
+            var orderIdIdentifier = "Dit ordrenummer er:";
+            var stationIdentifier = "Til rejsen:";
+            var priceIdentifyer = "Pris i alt:";
+            var departureTimeIdentifer = "Afgang";
+            var arrivalTimeIdentifer = "Ankomst";
+
+            return (IReciept)content;
         }
     }
+
+    
 }
