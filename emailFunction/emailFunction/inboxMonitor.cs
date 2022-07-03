2295165fe8ffa051a4a00a3e77afc500128d1fad
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-07-03 16:50:17 +0200

    Plz dont look at this

--- emailFunction/emailFunction/inboxMonitor.cs
+++ emailFunction/emailFunction/inboxMonitor.cs
@@ -21,24 +21,57 @@ namespace emailFunction
         {
             // Get the connection string from app settings and use it to create a connection.
             var str = Environment.GetEnvironmentVariable("sqldb_connection");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<Reciept>(requestBody);
+            data.price = data.price / 100;
+
             using (SqlConnection conn = new SqlConnection(str))
             {
-               
+
                 var commandText = "Insert into dbo.Reciept (depStation, arrStation, date, depTime, arrTime, price, orderId)" +
                     "value (@depStation, @arrStation, @date, @depTime, @arrTime, @price, @orderId)";
 
                 using (SqlCommand cmd = new SqlCommand(commandText, conn))
                 {
+                    SqlParameter depStation = new SqlParameter("@depStation", System.Data.SqlDbType.VarChar);
+                    depStation.Value = data.depStation;
+                    cmd.Parameters.Add(depStation);
 
-                    cmd.Parameters.Add("@depStation", SqlDbType.Int);
+                    SqlParameter arrStation = new SqlParameter("@arrStation", System.Data.SqlDbType.VarChar);
+                    arrStation.Value = data.arrStation;
+                    cmd.Parameters.Add(arrStation);
+
+                    SqlParameter date = new SqlParameter("@date", System.Data.SqlDbType.Date);
+                    date.Value = data.date;
+                    cmd.Parameters.Add(date);
+
+                    SqlParameter depTime = new SqlParameter("@depTime", System.Data.SqlDbType.Time);
+                    depTime.Value = data.depTime;
+                    cmd.Parameters.Add(depTime);
+
+                    SqlParameter arrTime = new SqlParameter("@arrTime", System.Data.SqlDbType.Time);
+                    arrTime.Value = data.arrTime;
+                    cmd.Parameters.Add(arrTime);
+
+                    SqlParameter price = new SqlParameter("@price", System.Data.SqlDbType.Int);
+                    price.Value = data.price;
+                    cmd.Parameters.Add(price);
+
+                    SqlParameter orderId = new SqlParameter("@orderId", System.Data.SqlDbType.Int);
+                    orderId.Value = data.orderId;
+                    cmd.Parameters.Add(orderId);
+
+
                     try
                     {
                         conn.Open();
 
-                        // Execute the command and log the # rows affected.
+                        // Execute the command.
                         await cmd.ExecuteNonQueryAsync();
                         log.LogInformation("New reciept added");
-                    } catch(Exception ex)
+                    }
+                    catch (Exception ex)
                     {
                         log.LogError(ex.ToString());
                     }
@@ -46,18 +79,18 @@ namespace emailFunction
 
 
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<Reciept>(requestBody);
-            data.price = data.price / 100;
-            log.LogInformation(data.price.ToString());
+                //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                //var data = JsonConvert.DeserializeObject<Reciept>(requestBody);
+                //data.price = data.price / 100;
+                //log.LogInformation(data.price.ToString());
 
-            //Insert Reciept into db
+                //Insert Reciept into db
 
 
-            return new OkResult();
+                return new OkResult();
+            }
+
         }
-
-    }
 
     public class Reciept
     {
