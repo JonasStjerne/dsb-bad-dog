4bdc76b881e0762688488b0393bdf362a53720d0
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-07-04 18:46:46 +0200

    Create time trigger function

---
+++ emailFunction/emailFunction/monitorJourneysFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace emailFunction
+{
+    public static class monitorJourneysFunction
+    {
+        [FunctionName("monitorJourneysFunction")]
+        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
