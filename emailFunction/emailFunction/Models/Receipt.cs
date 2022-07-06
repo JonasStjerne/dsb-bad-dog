338595f1456b9716097e13b29c930ef93656df70
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-07-06 14:31:38 +0200

    Models

---
+++ emailFunction/emailFunction/Models/Receipt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace emailFunction.Models
+{
+    public class Receipt
+    {
+            public string depStation { get; set; }
+            public string arrStation { get; set; }
+            public DateTime date { get; set; }
+            public TimeSpan depTime { get; set; }
+            public TimeSpan arrTime { get; set; }
+            public Double price { get; set; }
+            public string orderId { get; set; }
+    }
+}
---
+++ emailFunction/emailFunction/Models/StStopInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace service.Models
+{
+    public class StStopInformation
+    {
+        //StStopInformation(string name, string depTime, string depDate)
+        //{
+        //    name = Name;
+        //    depTime = DepTime;
+        //    depDate = DepDate;
+        //}
+        public string name { get; set; }
+        public string depTime { get; set; }
+        public string depDate { get; set; }
+
+    }
+}
