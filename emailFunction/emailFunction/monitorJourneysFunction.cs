03c9251e2616d5bef9953ddaae6d4acd001ed283
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-07-06 14:32:29 +0200

    Insert service logic into azure function

--- emailFunction/emailFunction/monitorJourneysFunction.cs
+++ emailFunction/emailFunction/monitorJourneysFunction.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using emailFunction.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +12,76 @@ namespace emailFunction
     public static class monitorJourneysFunction
     {
         [FunctionName("monitorJourneysFunction")]
-        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        public static async void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var delayTriggerInMinutes = 30;
+            List<Receipt> ElapsedTravels;
+            DateTime time = DateTime.Now;
+
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+
+            var commandText = String.Format("SELECT * FROM dbo.Reciept WHERE arrTime < {0}", time.AddMinutes(-delayTriggerInMinutes).ToString("HH:mm"));
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+
+                        // Execute the command.
+                        ElapsedTravels = await cmd.ExecuteNonQueryAsync();
+
+                        foreach (var journey in ElapsedTravels)
+                        {
+                            //Get ID's of depature station and destination station
+                            int originId = await RejseplanenService.LocationSerivce(journey.depStation);
+                            int destId = await RejseplanenService.LocationSerivce(journey.arrStation);
+
+                            //Get the link/request to journey that best matches the trip criteria
+                            var journeyInformationReq = await RejseplanenService.TripService(originId, destId, journey.arrTime.ToString("HH:mm"), journey.depTime.ToString("dd.MM.yy"));
+
+                            //Get more information about the specific journey found above
+                            dynamic journeyResponse = await RejseplanenService.JourneyDetailService(journeyInformationReq);
+
+                            //Find the destination station and retrieve the real time data. If the train is more than delayTrigger start refund otherwise ignore
+                            foreach (var stop in journeyResponse)
+                            {
+                                if (stop.name == journey.arrStation)
+                                {
+                                    
+                                    if (stop.rtTime)
+                                    {
+                                        DateTime rtData = DateTime.Parse(stop.rtTime);
+                                        DateTime plannedArr = DateTime.Parse(stop.time);
+
+                                        //Trigger refund if realtime data display more than delayTrigger delay and is at destinantion within 5 minutes
+                                        if (rtData > plannedArr.AddMinutes(delayTriggerInMinutes) && rtData < DateTime.Now.AddMinutes(5))
+                                        {
+                                            getRefund(journey.OrderId);
+                                            //Remember to delete Reciept from database when refund complete.
+                                        }
+                                    }
+                                    //If no realtime data, the train has arrived before delayTrigger, therefor delete
+                                    else
+                                    {
+                                        //Delete receipt, train arrived before delay trigger
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        log.LogError(ex.ToString());
+                    }
+                }
+            }
         }
     }
 }
